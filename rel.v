11fe45367b2353235ddbb73827b8194534701ce8
Damien Pous <damien.pous@ens-lyon.fr>
2021-10-08 14:59:43 +0200

    new helper lemmas to prove that contextual functions are below the companion (rel.unary_ctx_t)

--- rel.v
+++ rel.v
@@ -117,10 +117,15 @@ Section s.
 End s.
 
 
-(** * Contexts *)
+(** * Contexts closure functions *)
+
+Section context.
+Variable S: Type.
+Section unary. 
+Variable f: S -> S. 
 
 (** unary context: [unary_ctx f](R) = {(f x, f y) | x R y }  *)
-Program Definition unary_ctx S (f: S -> S): mon (S -> S -> Prop) :=
+Program Definition unary_ctx: mon (S -> S -> Prop) :=
   {| body R := sup_all (fun x =>
                sup (R x) (fun x' => pair (f x) (f x'))) |}.
 Next Obligation.
@@ -129,43 +134,64 @@ Next Obligation.
   Transparent plus. Opaque pair.
 Qed.
 
-Lemma leq_unary_ctx S f R T:
-  @unary_ctx S f R <= T <-> forall x x', R x x' -> T (f x) (f x').
+Lemma leq_unary_ctx R T:
+  unary_ctx R <= T <-> forall x x', R x x' -> T (f x) (f x').
 Proof.
   unfold unary_ctx. setoid_rewrite sup_all_spec.
   setoid_rewrite sup_spec.
   setoid_rewrite <-leq_pair. firstorder. 
 Qed.
-Lemma in_unary_ctx S (f: S -> S) (R: S -> S -> Prop) x x':
-  R x x' -> unary_ctx f R (f x) (f x').
+Lemma in_unary_ctx (R: S -> S -> Prop) x x':
+  R x x' -> unary_ctx R (f x) (f x').
 Proof. intros. now apply ->leq_unary_ctx. Qed.
 Global Opaque unary_ctx.
 
-(** such a function is always symmetric *)
-Lemma unary_sym S (f: S -> S): compat converse (unary_ctx f).
+(** context functions are always symmetric *)
+Lemma unary_sym: compat converse unary_ctx.
 Proof. intro R. simpl. apply leq_unary_ctx. intros. now apply in_unary_ctx. Qed.
 
+(** helper lemmas to prove that a unary context function is below the companion *)
+Lemma unary_ctx_b b c:
+  unary_ctx ° b <= c <-> forall R, Proper (b R ==> c R) f.
+Proof.
+  split; intro H.
+  - intros R p q pq. apply H. now apply in_unary_ctx.
+  - intro R. apply leq_unary_ctx. apply H.
+Qed.
+Corollary unary_ctx_t (b: mon (S -> S -> Prop)):
+  (forall R, Proper (b R ==> bT b unary_ctx R) f) ->
+  unary_ctx <= t b.
+Proof. intro H. apply Coinduction. now apply unary_ctx_b. Qed.
+Corollary unary_ctx_t_sym_ (b s: mon (S -> S -> Prop)) {H: Sym_from converse b s}:
+  (forall R, Proper (b R ==> s (T b unary_ctx R)) f) ->
+  unary_ctx <= t b.
+Proof. intro. apply Coinduction, by_Symmetry. apply unary_sym. now apply unary_ctx_b. Qed.
+
+
 (** if it is below [t], then the function [f] always preserves [t R], [bt R], [T f R], [bT f R] *)
-Lemma unary_proper_t S (f: S -> S) b:
-  unary_ctx f <= t b -> forall R, Proper (t b R ==> t b R) f.
-Proof. intros H R x x' Hx. apply (ft_t H). now apply (in_unary_ctx f). Qed.
-Lemma unary_proper_bt S (f: S -> S) b:
-  unary_ctx f <= t b -> forall R, Proper (bt b R ==> bt b R) f.
-Proof. intros H R x x' Hx. apply (fbt_bt H). now apply (in_unary_ctx f). Qed.
-Lemma unary_proper_T S (f: S -> S) b g:
-  unary_ctx f <= t b -> forall R, Proper (T b g R ==> T b g R) f.
-Proof. intros H R x x' Hx. apply (fT_T H). now apply (in_unary_ctx f). Qed.
-Lemma unary_proper_bT S (f: S -> S) b g:
-  unary_ctx f <= t b -> forall R, Proper (bT b g R ==> bT b g R) f.
-Proof. intros H R x x' Hx. apply (fbT_bT H). now apply (in_unary_ctx f). Qed.
+Lemma unary_proper_t b:
+  unary_ctx <= t b -> forall R, Proper (t b R ==> t b R) f.
+Proof. intros H R x x' Hx. apply (ft_t H). now apply in_unary_ctx. Qed.
+Lemma unary_proper_bt b:
+  unary_ctx <= t b -> forall R, Proper (bt b R ==> bt b R) f.
+Proof. intros H R x x' Hx. apply (fbt_bt H). now apply in_unary_ctx. Qed.
+Lemma unary_proper_T b g:
+  unary_ctx <= t b -> forall R, Proper (T b g R ==> T b g R) f.
+Proof. intros H R x x' Hx. apply (fT_T H). now apply in_unary_ctx. Qed.
+Lemma unary_proper_bT b g:
+  unary_ctx <= t b -> forall R, Proper (bT b g R ==> bT b g R) f.
+Proof. intros H R x x' Hx. apply (fbT_bT H). now apply in_unary_ctx. Qed.
 
-Global Instance unary_proper_Tctx S (f: S -> S) b R:
-  Proper (T b (unary_ctx f) R ==> T b (unary_ctx f) R) f.
-Proof. intros x x' Hx. apply (fTf_Tf b). now apply (in_unary_ctx f). Qed.
+Global Instance unary_proper_Tctx b R:
+  Proper (T b unary_ctx R ==> T b unary_ctx R) f.
+Proof. intros x x' Hx. apply (fTf_Tf b). now apply in_unary_ctx. Qed.
 
+End unary.
+Section binary. 
+Variable f: S -> S -> S. 
                                  
 (** binary context: [unary_ctx f](R) = {(f x x', f y y') | x R y, x' R y' }  *)
-Program Definition binary_ctx S (f: S -> S -> S): mon (S -> S -> Prop) :=
+Program Definition binary_ctx: mon (S -> S -> Prop) :=
   {| body R := sup_all (fun x =>
                sup_all (fun y => 
                sup (R x) (fun x' => 
@@ -176,43 +202,64 @@ Next Obligation.
   Transparent plus. Opaque pair.
 Qed.
 
-Lemma leq_binary_ctx S f R T:
-  @binary_ctx S f R <= T <-> forall x x', R x x' -> forall y y', R y y' -> T (f x y) (f x' y').
+Lemma leq_binary_ctx R T:
+  binary_ctx R <= T <-> forall x x', R x x' -> forall y y', R y y' -> T (f x y) (f x' y').
 Proof.
   unfold binary_ctx. do 2 setoid_rewrite sup_all_spec.
   do 2 setoid_rewrite sup_spec.
   setoid_rewrite <-leq_pair. firstorder. 
 Qed.
-Lemma in_binary_ctx S (f: S -> S -> S) (R: S -> S -> Prop) x x' y y':
-  R x x' -> R y y' -> binary_ctx f R (f x y) (f x' y').
+Lemma in_binary_ctx (R: S -> S -> Prop) x x' y y':
+  R x x' -> R y y' -> binary_ctx R (f x y) (f x' y').
 Proof. intros. now apply ->leq_binary_ctx. Qed.
 Global Opaque binary_ctx.
 
 (** such a function is always symmetric *)
-Lemma binary_sym S (f: S -> S -> S): compat converse (binary_ctx f).
+Lemma binary_sym: compat converse binary_ctx.
 Proof. intro R. simpl. apply leq_binary_ctx. intros. now apply in_binary_ctx. Qed.
 
-(** if it is below [t], then the function [f] always preserves [t R], [bt R], [T f R], [bT f R] *)
-Lemma binary_proper_t S (f: S -> S -> S) b:
-  binary_ctx f <= t b -> forall R, Proper (t b R ==> t b R ==> t b R) f.
-Proof. intros H R x x' Hx y y' Hy. apply (ft_t H). now apply (in_binary_ctx f). Qed.
-Lemma binary_proper_bt S (f: S -> S -> S) b:
-  binary_ctx f <= t b -> forall R, Proper (bt b R ==> bt b R ==> bt b R) f.
-Proof. intros H R x x' Hx y y' Hy. apply (fbt_bt H). now apply (in_binary_ctx f). Qed.
-Lemma binary_proper_T S (f: S -> S -> S) b g:
-  binary_ctx f <= t b -> forall R, Proper (T b g R ==> T b g R ==> T b g R) f.
-Proof. intros H R x x' Hx y y' Hy. apply (fT_T H). now apply (in_binary_ctx f). Qed.
-Lemma binary_proper_bT S (f: S -> S -> S) b g:
-  binary_ctx f <= t b -> forall R, Proper (bT b g R ==> bT b g R ==> bT b g R) f.
-Proof. intros H R x x' Hx y y' Hy. apply (fbT_bT H). now apply (in_binary_ctx f). Qed.
+(** helper lemmas to prove that a binary context function is below the companion *)
+Lemma binary_ctx_b b c:
+  binary_ctx ° b <= c  <-> forall R, Proper (b R ==> b R ==> c R) f.
+Proof.
+  split; intro H.
+  - intros R p q pq u v uv. apply H. now apply in_binary_ctx.
+  - intro R. apply leq_binary_ctx. apply H.
+Qed.
+Corollary binary_ctx_t (b: mon (S -> S -> Prop)):
+  (forall R, Proper (b R ==> b R ==> bT b binary_ctx R) f) ->
+  binary_ctx <= t b.
+Proof. intro H. apply Coinduction. now apply binary_ctx_b. Qed.
+Corollary binary_ctx_t_sym_ (b s: mon (S -> S -> Prop)) {H: Sym_from converse b s}:
+  (forall R, Proper (b R ==> b R ==> s (T b binary_ctx R)) f) ->
+  binary_ctx <= t b.
+Proof. intro. apply Coinduction, by_Symmetry. apply binary_sym. now apply binary_ctx_b. Qed.
 
-Global Instance binary_proper_Tctx S (f: S -> S -> S) b R:
-  Proper (T b (binary_ctx f) R ==> T b (binary_ctx f) R ==> T b (binary_ctx f) R) f.
-Proof. intros x x' Hx y y' Hy. apply (fTf_Tf b). now apply (in_binary_ctx f). Qed.
+(** if it is below [t], then the function [f] always preserves [t R], [bt R], [T f R], [bT f R] *)
+Lemma binary_proper_t b:
+  binary_ctx <= t b -> forall R, Proper (t b R ==> t b R ==> t b R) f.
+Proof. intros H R x x' Hx y y' Hy. apply (ft_t H). now apply in_binary_ctx. Qed.
+Lemma binary_proper_bt b:
+  binary_ctx <= t b -> forall R, Proper (bt b R ==> bt b R ==> bt b R) f.
+Proof. intros H R x x' Hx y y' Hy. apply (fbt_bt H). now apply in_binary_ctx. Qed.
+Lemma binary_proper_T b g:
+  binary_ctx <= t b -> forall R, Proper (T b g R ==> T b g R ==> T b g R) f.
+Proof. intros H R x x' Hx y y' Hy. apply (fT_T H). now apply in_binary_ctx. Qed.
+Lemma binary_proper_bT b g:
+  binary_ctx <= t b -> forall R, Proper (bT b g R ==> bT b g R ==> bT b g R) f.
+Proof. intros H R x x' Hx y y' Hy. apply (fbT_bT H). now apply in_binary_ctx. Qed.
+
+Global Instance binary_proper_Tctx b R:
+  Proper (T b binary_ctx R ==> T b binary_ctx R ==> T b binary_ctx R) f.
+Proof. intros x x' Hx y y' Hy. apply (fTf_Tf b). now apply in_binary_ctx. Qed.
+
+End binary.
+Section ternary. 
+Variable f: S -> S -> S -> S. 
 
                                  
-(** ternary context: [unary_ctx f](R) = {(f x x', f y y') | x R y, x' R y' }  *)
-Program Definition ternary_ctx S (f: S -> S -> S -> S): mon (S -> S -> Prop) :=
+(** ternary context: [ternary_ctx f](R) = {(f x x' x'', f y y' y'') | x R y, x' R y', x'' R y'' }  *)
+Program Definition ternary_ctx: mon (S -> S -> Prop) :=
   {| body R := sup_all (fun x =>
                sup_all (fun y => 
                sup_all (fun z => 
@@ -225,40 +272,65 @@ Next Obligation.
   Transparent plus. Opaque pair.
 Qed.
 
-Lemma leq_ternary_ctx S f R T:
-  @ternary_ctx S f R <= T <->
+Lemma leq_ternary_ctx R T:
+  ternary_ctx R <= T <->
   forall x x', R x x' -> forall y y', R y y' -> forall z z', R z z' -> T (f x y z) (f x' y' z').
 Proof.
   unfold ternary_ctx. do 3 setoid_rewrite sup_all_spec.
   do 3 setoid_rewrite sup_spec.
   setoid_rewrite <-leq_pair. firstorder. 
 Qed.
-Lemma in_ternary_ctx S (f: S -> S -> S -> S) (R: S -> S -> Prop) x x' y y' z z':
-  R x x' -> R y y' -> R z z' -> ternary_ctx f R (f x y z) (f x' y' z').
+Lemma in_ternary_ctx (R: S -> S -> Prop) x x' y y' z z':
+  R x x' -> R y y' -> R z z' -> ternary_ctx R (f x y z) (f x' y' z').
 Proof. intros. now apply ->leq_ternary_ctx. Qed.
 Global Opaque ternary_ctx.
 
 (** such a function is always symmetric *)
-Lemma ternary_sym S (f: S -> S -> S -> S): compat converse (ternary_ctx f).
+Lemma ternary_sym: compat converse ternary_ctx.
 Proof. intro R. simpl. apply leq_ternary_ctx. intros. now apply in_ternary_ctx. Qed.
 
-(** if it is below [t], then the function [f] always preserves [t R], [bt R], [T f R], [bT f R] *)
-Lemma ternary_proper_t S (f: S -> S -> S -> S) b:
-  ternary_ctx f <= t b -> forall R, Proper (t b R ==> t b R ==> t b R ==> t b R) f.
-Proof. intros H R x x' Hx y y' Hy z z' Hz. apply (ft_t H). now apply (in_ternary_ctx f). Qed.
-Lemma ternary_proper_bt S (f: S -> S -> S -> S) b:
-  ternary_ctx f <= t b -> forall R, Proper (bt b R ==> bt b R ==> bt b R ==> bt b R) f.
-Proof. intros H R x x' Hx y y' Hy z z' Hz. apply (fbt_bt H). now apply (in_ternary_ctx f). Qed.
-Lemma ternary_proper_T S (f: S -> S -> S -> S) b g:
-  ternary_ctx f <= t b -> forall R, Proper (T b g R ==> T b g R ==> T b g R ==> T b g R) f.
-Proof. intros H R x x' Hx y y' Hy z z' Hz. apply (fT_T H). now apply (in_ternary_ctx f). Qed.
-Lemma ternary_proper_bT S (f: S -> S -> S -> S) b g:
-  ternary_ctx f <= t b -> forall R, Proper (bT b g R ==> bT b g R ==> bT b g R ==> bT b g R) f.
-Proof. intros H R x x' Hx y y' Hy z z' Hz. apply (fbT_bT H). now apply (in_ternary_ctx f). Qed.
+(** helper lemmas to prove that a ternary context function is below the companion *)
+Lemma ternary_ctx_b b c:
+  ternary_ctx ° b <= c <-> forall R, Proper (b R ==> b R ==> b R ==> c R) f.
+Proof.
+  split; intro H.
+  - intros R p q pq u v uv s t st. apply H. now apply in_ternary_ctx.
+  - intro R. apply leq_ternary_ctx. apply H.
+Qed.
+Corollary ternary_ctx_t (b: mon (S -> S -> Prop)):
+  (forall R, Proper (b R ==> b R ==> b R ==> bT b ternary_ctx R) f) ->
+  ternary_ctx <= t b.
+Proof. intro H. apply Coinduction. now apply ternary_ctx_b. Qed.
+Corollary ternary_ctx_t_sym_ (b s: mon (S -> S -> Prop)) {H: Sym_from converse b s}:
+  (forall R, Proper (b R ==> b R ==> b R ==> s (T b ternary_ctx R)) f) ->
+  ternary_ctx <= t b.
+Proof. intro. apply Coinduction, by_Symmetry. apply ternary_sym. now apply ternary_ctx_b. Qed.
 
-Global Instance ternary_proper_Tctx S (f: S -> S -> S -> S) b R:
-  Proper (T b (ternary_ctx f) R ==> T b (ternary_ctx f) R ==> T b (ternary_ctx f) R ==> T b (ternary_ctx f) R) f.
-Proof. intros x x' Hx y y' Hy z z' Hz. apply (fTf_Tf b). now apply (in_ternary_ctx f). Qed.
+(** if it is below [t], then the function [f] always preserves [t R], [bt R], [T f R], [bT f R] *)
+Lemma ternary_proper_t b:
+  ternary_ctx <= t b -> forall R, Proper (t b R ==> t b R ==> t b R ==> t b R) f.
+Proof. intros H R x x' Hx y y' Hy z z' Hz. apply (ft_t H). now apply in_ternary_ctx. Qed.
+Lemma ternary_proper_bt b:
+  ternary_ctx <= t b -> forall R, Proper (bt b R ==> bt b R ==> bt b R ==> bt b R) f.
+Proof. intros H R x x' Hx y y' Hy z z' Hz. apply (fbt_bt H). now apply in_ternary_ctx. Qed.
+Lemma ternary_proper_T b g:
+  ternary_ctx <= t b -> forall R, Proper (T b g R ==> T b g R ==> T b g R ==> T b g R) f.
+Proof. intros H R x x' Hx y y' Hy z z' Hz. apply (fT_T H). now apply in_ternary_ctx. Qed.
+Lemma ternary_proper_bT b g:
+  ternary_ctx <= t b -> forall R, Proper (bT b g R ==> bT b g R ==> bT b g R ==> bT b g R) f.
+Proof. intros H R x x' Hx y y' Hy z z' Hz. apply (fbT_bT H). now apply in_ternary_ctx. Qed.
+
+Global Instance ternary_proper_Tctx b R:
+  Proper (T b ternary_ctx R ==> T b ternary_ctx R ==> T b ternary_ctx R ==> T b ternary_ctx R) f.
+Proof. intros x x' Hx y y' Hy z z' Hz. apply (fTf_Tf b). now apply in_ternary_ctx. Qed.
+
+End ternary.
+End context.
+
+(** reexporting those three lemmas with different argument order to help typeclasss resolution *)
+Definition unary_ctx_t_sym {S b s H} f := @unary_ctx_t_sym_ S f b s H. 
+Definition binary_ctx_t_sym {S b s H} f := @binary_ctx_t_sym_ S f b s H. 
+Definition ternary_ctx_t_sym {S b s H} f := @ternary_ctx_t_sym_ S f b s H. 
 
 Transparent pair.
 Lemma converse_pair A (p q: A): converse (pair p q) == pair q p.
